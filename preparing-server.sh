c2e49e84016c8e7bab967e276aa7dffdf2afdb24
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-08-02 17:45:07 +0300

    Update preparing-server.sh

--- preparing-server.sh
+++ preparing-server.sh
@@ -4,3 +4,13 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install curl tar wget tmux htop net-tools clang pkg-config libssl-dev jq build-essential git screen make ncdu -y
 exec > /dev/tty 2>&1
 sleep 1
+exec > /dev/null 2>&1
+cd $HOME
+wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
+echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
+echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
+rm -rf go1.20.4.linux-amd64.tar.gz
+exec > /dev/tty 2>&1
