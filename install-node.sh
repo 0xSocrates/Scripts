985439329594d8f9ef046ce4b02839054bd6d7d0
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-08-02 18:32:10 +0300

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -17,8 +17,8 @@ ge_ad_se_pe() {
 exec > /dev/null 2>&1
 curl -Ls https://raw.githubusercontent.com/Core-Node-Team/Testnet-TR/main/---/addrbook.json > $HOME/$DirectName/config/addrbook.json
 curl -Ls https://raw.githubusercontent.com/Core-Node-Team/Testnet-TR/main/----/genesis.json > $HOME/$DirectName/config/genesis.json
-peers=" "
-seeds=" "
+peers=""
+seeds=""
 sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/$DirectName/config/config.toml
 # min gas price
 exec > /dev/tty 2>&1
