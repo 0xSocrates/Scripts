e055add953669fbbb98f448f23a4f8445cb44700
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-08-02 17:49:12 +0300

    Update preparing-server.sh

--- preparing-server.sh
+++ preparing-server.sh
@@ -2,9 +2,6 @@
 exec > /dev/null 2>&1
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install curl tar wget tmux htop net-tools clang pkg-config libssl-dev jq build-essential git screen make ncdu -y
-exec > /dev/tty 2>&1
-sleep 1
-exec > /dev/null 2>&1
 cd $HOME
 wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
