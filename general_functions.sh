04bdacca0688cce51f23c62ae1bbc3078f2442fa
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-12-12 00:52:05 +0300

    Create general_functions.sh

---
+++ general_functions.sh
@@ -0,0 +1,92 @@
+
+get_moniker () {
+echo -e '\e[0;35m' && read -p "Moniker isminizi girin: " MONIKER && echo -e '\e[0m' && echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+sleep 1
+}
+
+init() {
+$BinaryName config chain-id $ChainID
+$BinaryName config keyring-backend test
+$BinaryName config node tcp://localhost:${CustomPort}57
+$BinaryName init $MONIKER --chain-id $ChainID
+print_color $Yellow "$BinaryName Başlatıldı."
+}
+
+removenode() {
+exec > /dev/null 2>&1
+sudo systemctl stop $BinaryName && \
+sudo systemctl disable $BinaryName && \
+rm /etc/systemd/system/$BinaryName.service && \
+sudo systemctl daemon-reload && \
+cd $HOME && \
+rm -rf $DirectName && \
+rm -rf $NodeName && \
+rm -rf $(which $BinaryName)
+exec > /dev/tty 2>&1
+}
+
+prepare_server() {
+print_color $Blue "Sunucu Hazırlanıyor..."
+sleep 1
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install curl tar wget tmux htop net-tools clang pkg-config libssl-dev jq build-essential git screen make ncdu liblz4-tool -y
+cd $HOME
+wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
+echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
+echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
+rm -rf go1.20.4.linux-amd64.tar.gz
+source $HOME/.bash_profile
+print_color $Yellow "Güncellendi, Kütüphaneler Kuruldu, go version go1.20.4 linux/amd64 Kuruldu"
+}
+
+
+logo() {
+echo -e '\e[0;32m'
+echo " ▄████████  ▄██████▄     ▄████████    ▄████████     ███▄▄▄▄    ▄██████▄  ████████▄     ▄████████ ";
+echo "███    ███ ███    ███   ███    ███   ███    ███     ███▀▀▀██▄ ███    ███ ███   ▀███   ███    ███ ";
+echo "███    █▀  ███    ███   ███    ███   ███    █▀      ███   ███ ███    ███ ███    ███   ███    █▀  ";
+echo "███        ███    ███  ▄███▄▄▄▄██▀  ▄███▄▄▄         ███   ███ ███    ███ ███    ███  ▄███▄▄▄     ";
+echo "███        ███    ███ ▀▀███▀▀▀▀▀   ▀▀███▀▀▀         ███   ███ ███    ███ ███    ███ ▀▀███▀▀▀     ";
+echo "███    █▄  ███    ███ ▀███████████   ███    █▄      ███   ███ ███    ███ ███    ███   ███    █▄  ";
+echo "███    ███ ███    ███   ███    ███   ███    ███     ███   ███ ███    ███ ███   ▄███   ███    ███ ";
+echo "████████▀   ▀██████▀    ███    ███   ██████████      ▀█   █▀   ▀██████▀  ████████▀    ██████████ ";
+echo "                        ███    ███                                                               ";
+echo ""
+echo -e '\e[0m' 
+echo -e ''
+echo -e ''
+sleep 4
+print_color $Blue "$NodeName Kurulumu Başlatılıyor..."
+sleep 2
+echo -e ''
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
