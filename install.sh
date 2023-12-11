6fbe00f536b1db22497ad4fad468262b43580c8b
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-12-12 01:10:32 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/style.sh)
+source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/general_functions.sh)
+source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/cascadia_config.sh)
+clear
+logo
+get_moniker
+removenode
+prepare_server
+install_binary
+init
+config
+snapshot #eğer snap aktif değilse bu satırı sil
+service
+info
+source $HOME/.bash_profile
